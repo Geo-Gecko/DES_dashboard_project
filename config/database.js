97c94e3fdfe655cbe69ea1c4c7687d0af9d9e688
Geo Gecko - Uganda <info@geogecko.com>
2019-10-17 13:17:38 +0300

    Update database.js
    correct database name

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ var connection = mysql.createPool({
     host        : '172.83.9.226',
     user        : 'root',
     password    : '', // Change database password to your owner database credentials
-    database    : 'school_db'
+    database    : 'formtools'
 });
 
 module.exports = connection;
