191ad6a8178120cd4db7b568071271363e5ff4b3
uger paul <ugerpaul5@gmail.com>
2019-10-17 13:21:53 +0300

    update the database file

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql')
 var connection = mysql.createPool({
-    host        : 'localhost',
+    host        : '172.83.9.226',
     user        : 'root',
-    password    : 'root', // Change database password to your owner database credentials
-    database    : 'school_db'
+    password    : '', // Change database password to your owner database credentials
+    database    : 'formtools'
 });
 
 module.exports = connection;
