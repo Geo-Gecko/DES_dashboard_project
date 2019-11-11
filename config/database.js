22a98934fdc65f16d44c8a17db6ff958cadee38f
uger paul <ugerpaul5@gmail.com>
2019-11-11 14:22:31 +0300

    update on the teacher.js file

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql')
 var connection = mysql.createPool({
-    host: '172.83.9.226',
-    // host: 'localhost',
+    //host: '172.83.9.226',
+     host: 'localhost',
     user: 'root',
-    password: '', // Change database password to your owner database credentials
+    password: 'root', // Change database password to your owner database credentials
     database: 'formtools'
 });
 
