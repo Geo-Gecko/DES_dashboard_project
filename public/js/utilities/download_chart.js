c14db3a52b1b9bfb1a12c0ee720da85763de8947
RonKbS <stephen.bamwite@gmail.com>
2020-02-10 14:23:23 +0300

    add download fn_ality to other pages

--- public/js/utilities/download_chart.js
+++ public/js/utilities/download_chart.js
@@ -5,7 +5,15 @@ let downloadChart = function (element) {
     window.scrollTo(0,0);
     let chartDiv = element.parentNode.children[1]
     let chart_title = chartDiv.children[0].innerText.replace(" ", "")
-    html2canvas(chartDiv, { height: chartDiv.scrollHeight, width: chartDiv.scrollWidth })
+    html2canvas(
+        chartDiv,
+        {
+            height: chartDiv.scrollHeight,
+            width: chartDiv.scrollWidth,
+            allowTaint: true,
+            useCORS: true
+        }
+    )
         .then(canvas => {
             let theCanvas = canvas;
             theCanvas.toBlob(blob => {
